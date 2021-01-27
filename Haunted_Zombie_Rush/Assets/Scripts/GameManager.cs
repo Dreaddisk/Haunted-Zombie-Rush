947e4980a16d5de4c60b5857f85f8fca0f487e3c
Jose <joey_manuel@outlook.com>
2021-01-27 05:36:05 -0500

    2.GameMAnager
    Finished the GameManager.cs script

---
+++ Haunted_Zombie_Rush/Assets/Scripts/GameManager.cs
@@ -0,0 +1,67 @@
+﻿
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public class GameManager : MonoBehaviour
+{
+
+    #region Variables
+    public static GameManager instance = null;
+
+    [SerializeField]
+    private GameObject mainMenu;
+
+    private bool playerActive = false;
+    private bool gameOver = false;
+    private bool gameStarted = false;
+
+    public bool PlayerActive
+    {
+        get { return playerActive; }
+    }
+
+    public bool GameOver
+    {
+        get { return gameOver; }
+    }
+
+    public bool GameStarted
+    {
+        get { return gameStarted; }
+    }
+    #endregion Variables
+
+
+
+    #region UnityFunctions
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+
+        DontDestroyOnLoad(gameObject);
+
+        Assert.IsNotNull(mainMenu);
+    }
+
+    #endregion UnityFunctions
+
+    public void PlayerCollided()
+    {
+        gameOver = true;
+    }
+
+    public void PlayerStartedGame()
+    {
+        playerActive = true;
+    }
+
+    public void EnterGame()
+    {
+        mainMenu.SetActive(false);
+        gameStarted = true;
+    }
+
+}// GameManager class
